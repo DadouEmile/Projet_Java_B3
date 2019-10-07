bf117ca814389d28c732bcb8c81ae2b4ede0f349
emile <emile.dadou@protonmail.com>
2019-10-07 10:31:45 +0200

    retrieve screen size and logo display

--- src/samples/Main.java
+++ src/samples/Main.java
@@ -4,17 +4,29 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
+import javafx.geometry.Rectangle2D;
+import javax.tools.Tool;
+import java.awt.*;
 
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
+        //get the screen display size
+        Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+        int screenHeight = screenSize.height;
+        int screenWidth = screenSize.width;
+
+        //load graphics for the app window
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+
+        // Display the app window
+        primaryStage.setTitle("SecuriScan");
+        primaryStage.setScene(new Scene(root, screenWidth, screenHeight));
+        primaryStage.getIcons().add( new Image( Main.class.getResourceAsStream( "icon.png" )));
         primaryStage.show();
-        System.out.println("hello");
     }
 
 

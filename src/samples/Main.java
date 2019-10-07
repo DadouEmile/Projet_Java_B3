788d6d769efa29478511bbe7c87493904a1e2bb9
emile <emile.dadou@protonmail.com>
2019-10-07 12:19:23 +0200

    Add opencv support and test on capture the video flow with opencv (not working right now)

--- src/samples/Main.java
+++ src/samples/Main.java
@@ -1,36 +1,43 @@
 package samples;
 
+import org.opencv.core.Core;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.stage.Stage;
-import javafx.geometry.Rectangle2D;
-import javax.tools.Tool;
 import java.awt.*;
 
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        //get the screen display size
-        Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
-        int screenHeight = screenSize.height;
-        int screenWidth = screenSize.width;
+        try {
+            //get the screen display size
+            Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+            int screenHeight = screenSize.height;
+            int screenWidth = screenSize.width;
 
-        //load graphics for the app window
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+            //load graphics for the app window
+            Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
 
-        // Display the app window
-        primaryStage.setTitle("SecuriScan");
-        primaryStage.setScene(new Scene(root, screenWidth, screenHeight));
-        primaryStage.getIcons().add( new Image( Main.class.getResourceAsStream( "icon.png" )));
-        primaryStage.show();
+            // Display the app window
+            primaryStage.setTitle("SecuriScan");
+            primaryStage.setScene(new Scene(root, screenWidth, screenHeight));
+            primaryStage.getIcons().add(new Image(Main.class.getResourceAsStream("icon.png")));
+
+            //display graphics
+            primaryStage.show();
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
 
     public static void main(String[] args) {
+        System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
         launch(args);
     }
 }
